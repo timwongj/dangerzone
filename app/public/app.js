7f2a710091207813e8c2b629702a55e1f347ba28
timwongj <wong.t@husky.neu.edu>
2017-10-28 23:41:26 -0400

    added google maps

--- app/public/app.js
+++ app/public/app.js
@@ -4,7 +4,13 @@
 
   var app = angular.module('dangerZoneApp', ['ngResource']);
 
-  app.controller('DangerZoneController', function($scope, $resource) {
+  app.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      'https://maps.google.com/**']);
+  });
+
+  app.controller('DangerZoneController', function($scope, $resource, $sce) {
 
     var Crimes = $resource('/crimes');
 
@@ -13,6 +19,7 @@
     navigator.geolocation.getCurrentPosition(function(pos) {
       $scope.lat = pos.coords.latitude;
       $scope.long = pos.coords.longitude;
+      $scope.mapLink = 'https://maps.google.com/maps?q=' + $scope.lat + ',' + $scope.long + '&output=embed';
       $scope.crimes = Crimes.query({ lat: $scope.lat, long: $scope.long }, function() {
         $scope.loaded = true;
       });
